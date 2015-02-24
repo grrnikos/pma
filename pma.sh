ea45112e60affe65f5c967a68f0330f231f7bc2f
Nikos Gr <grrnikos@yahoo.gr>
2015-02-24 19:50:18 +0200

    update phpMyadmin to 4.3.10

--- pma.sh
+++ pma.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo 'Downloading PhpMyAdmin 4.3.9'
-curl -#L http://sourceforge.net/projects/phpmyadmin/files/phpMyAdmin/4.3.9/phpMyAdmin-4.3.9-english.tar.gz -o phpmyadmin.tar.gz
+echo 'Downloading PhpMyAdmin 4.3.10'
+curl -#L http://sourceforge.net/projects/phpmyadmin/files/phpMyAdmin/4.3.10/phpMyAdmin-4.3.10-english.tar.gz -o phpmyadmin.tar.gz
 
 mkdir phpmyadmin && tar xf phpmyadmin.tar.gz -C phpmyadmin --strip-components 1
 
