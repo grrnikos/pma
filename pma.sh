2a01c59c1e4969851a65c4ae598d7013ff6f0f98
Nikos Gr <grrnikos@yahoo.gr>
2015-04-29 19:25:09 +0300

    update phpMyAdmin to 4.4.4

--- pma.sh
+++ pma.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo 'Downloading PhpMyAdmin 4.4.3'
-curl -#L http://sourceforge.net/projects/phpmyadmin/files/phpMyAdmin/4.4.3/phpMyAdmin-4.4.3-english.tar.gz -o phpmyadmin.tar.gz
+echo 'Downloading PhpMyAdmin 4.4.4'
+curl -#L http://sourceforge.net/projects/phpmyadmin/files/phpMyAdmin/4.4.4/phpMyAdmin-4.4.4-english.tar.gz -o phpmyadmin.tar.gz
 
 mkdir phpmyadmin && tar xf phpmyadmin.tar.gz -C phpmyadmin --strip-components 1
 
