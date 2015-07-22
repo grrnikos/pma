a32066f2d8047439684cfd7a4cdf9f879b2eff7f
abshammeri <abshammeri@gmail.com>
2015-07-22 15:45:57 +0300

    Fixed : URL issue
    phpmyadmin downloads moved from sourceforge to phpmyadmin.net

--- pma.sh
+++ pma.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo 'Downloading PhpMyAdmin 4.4.11'
-curl -#L http://sourceforge.net/projects/phpmyadmin/files/phpMyAdmin/4.4.11/phpMyAdmin-4.4.11-english.tar.gz -o phpmyadmin.tar.gz
+curl -#L https://files.phpmyadmin.net/phpMyAdmin/4.4.12/phpMyAdmin-4.4.12-english.tar.gz -o phpmyadmin.tar.gz
 
 mkdir phpmyadmin && tar xf phpmyadmin.tar.gz -C phpmyadmin --strip-components 1
 
