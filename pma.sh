05d8bddf76ceecaecf20d0db3423df94ecf2efa5
abshammeri <abshammeri@gmail.com>
2015-07-26 14:15:00 +0300

    Update pma.sh

--- pma.sh
+++ pma.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'Downloading PhpMyAdmin 4.4.11'
+echo 'Downloading PhpMyAdmin 4.4.12'
 curl -#L https://files.phpmyadmin.net/phpMyAdmin/4.4.12/phpMyAdmin-4.4.12-english.tar.gz -o phpmyadmin.tar.gz
 
 mkdir phpmyadmin && tar xf phpmyadmin.tar.gz -C phpmyadmin --strip-components 1
