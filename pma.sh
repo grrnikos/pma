5365cd031f47255655fe7fde6933081241ade9aa
Nikos Gr <grrnikos@yahoo.gr>
2015-12-06 08:54:45 +0200

    update phpMyAdmin to 4.5.2

--- pma.sh
+++ pma.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo 'Downloading PhpMyAdmin 4.4.14'
-curl -#L https://files.phpmyadmin.net/phpMyAdmin/4.4.14/phpMyAdmin-4.4.14-english.tar.gz -o phpmyadmin.tar.gz
+echo 'Downloading PhpMyAdmin 4.5.2'
+curl -#L https://files.phpmyadmin.net/phpMyAdmin/4.5.2/phpMyAdmin-4.5.2-english.tar.gz -o phpmyadmin.tar.gz
 
 mkdir phpmyadmin && tar xf phpmyadmin.tar.gz -C phpmyadmin --strip-components 1
 
