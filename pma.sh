f718b8eb033b61289460629f224efa724444e110
Manish Motwani <motsmanish@users.noreply.github.com>
2017-01-10 16:49:38 +0530

    Updated version.

--- pma.sh
+++ pma.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo 'Downloading phpMyAdmin 4.6.4'
-curl -#L https://files.phpmyadmin.net/phpMyAdmin/4.6.4/phpMyAdmin-4.6.4-english.tar.gz -o phpmyadmin.tar.gz
+echo 'Downloading phpMyAdmin 4.6.5.2 (English language only)'
+curl -#L https://files.phpmyadmin.net/phpMyAdmin/4.6.5.2/phpMyAdmin-4.6.5.2-english.tar.gz -o phpmyadmin.tar.gz
 
 mkdir phpmyadmin && tar xf phpmyadmin.tar.gz -C phpmyadmin --strip-components 1
 
