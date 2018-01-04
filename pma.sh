a2dd59df513151b5d44c2d5b10067eed28b1f494
Nathan Soares <nathanosoares@users.noreply.github.com>
2018-01-04 16:23:49 -0200

    Update pma.sh
    Update to 4.7.7

--- pma.sh
+++ pma.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo 'Downloading phpMyAdmin 4.7.6'
-curl -#L https://files.phpmyadmin.net/phpMyAdmin/4.7.6/phpMyAdmin-4.7.6-english.tar.gz -o phpmyadmin.tar.gz
+echo 'Downloading phpMyAdmin 4.7.7'
+curl -#L https://files.phpmyadmin.net/phpMyAdmin/4.7.7/phpMyAdmin-4.7.7-english.tar.gz -o phpmyadmin.tar.gz
 
 mkdir phpmyadmin && tar xf phpmyadmin.tar.gz -C phpmyadmin --strip-components 1
 
